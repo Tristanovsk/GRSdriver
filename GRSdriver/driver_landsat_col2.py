fix brightness temp units of TIR bands

GRSdriver/driver_landsat_col2.py
@@ -114,7 +114,11 @@ class LandsatDriver():
         # -----------------------------------------------------------
         # convert band from normalized radiance into reflectance
         # -----------------------------------------------------------
-        self.prod['bands']=self.prod['bands'] / np.cos(np.radians(self.prod['sza']))
+        mus = np.cos(np.radians(self.prod['sza']))
+        for wl, band in self.prod.bands.groupby('wl'):
+            # convert into reflectance vis-NIR-SWIR bands (upper limit 3000nm)
+            if float(wl) <= 3000:
+                self.prod['bands'].loc[{'wl':wl}]= band / mus
 
 
         self.prod.attrs = self.prod.attrs